78d272bfeb28e38b06cdfa28eb50fe7c27164738
MetConMG <152808713+digital-Alchemy-mcmg@users.noreply.github.com>
2026-09-07 23:43:04 -0400

    Implement MultiStateManager for multiple active states
    Refactor MultiStateManager to manage multiple active states with timestamps.

--- src/state/multi_state.py
+++ src/state/multi_state.py
@@ -1,11 +1,34 @@
-# C03: Multi-State Manager (DRAFT - CONTAINS ERRORS)
-# TODO: Fix multi-state coexistence and timing gates.
+import time
+
 class MultiStateManager:
-    def __init__(self, initial_state):
-        self.current_state = initial_state
+    def __init__(self):
+        self.active_states = set()
+        self.state_timestamps = {}
 
-    def set_state(self, state):
-        self.current_state = state
+    def add_state(self, state):
+        if state not in self.active_states:
+            self.active_states.add(state)
+            self.state_timestamps[state] = time.time()
+            return True
+        return False
 
-    def check_timing_gate(self, last_seen):
-        pass
+    def remove_state(self, state):
+        if state in self.active_states:
+            self.active_states.remove(state)
+            self.state_timestamps.pop(state, None)
+            return True
+        return False
+
+    def is_active(self, state):
+        return state in self.active_states
+
+    def get_elapsed_time(self, state):
+        if state in self.state_timestamps:
+            return time.time() - self.state_timestamps[state]
+        return 0
+
+    def can_transition_to_offline(self, threshold_seconds):
+        if not self.is_active("UNAVAILABLE"):
+            return False
+        elapsed = self.get_elapsed_time("UNAVAILABLE")
+        return elapsed >= threshold_seconds
